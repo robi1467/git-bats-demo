c1fa67942e0124dfb4807c63b7411a38f975637c
robi1467 <robi1467@morris.umn.edu>
2020-08-21 18:57:55 -0500

    Complete the file count_successes.sh
    Methods to count successes and failures was implemented into the file. All of the test pass
    ...atleast they said they did, I am currently unable to check shellcheck I enter the command
    and nothing happened so that was cool.

--- count_successes.sh
+++ count_successes.sh
@@ -1,2 +1,14 @@
 #!/usr/bin/env bash
-echo "There were 78 successes and 22 failures."
+
+tar_file=$1
+
+SCRATCH=$(mktemp -d)
+
+tar -zxf "$tar_file" --directory="$SCRATCH"
+
+num_successes=$(grep -r -l "SUCCESS" "$SCRATCH" | wc -l)
+num_failures=$(grep -r -l "FAILURE" "$SCRATCH" | wc -l)
+
+echo "There were $num_successes successes and $num_failures failures."
+
+rm -rf "$SCRATCH"
